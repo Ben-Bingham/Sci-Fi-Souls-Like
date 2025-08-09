2913899550288499dbc46aa8f3f740eee35dc102
Ben Bingham <ben@benbingham.ca>
2025-08-09 15:45:21 -0600

    Added basic weapon swing

--- player.gd
+++ player.gd
@@ -2,26 +2,34 @@ extends CharacterBody2D
 
 @export var moveSpeed = 250.0;
 
-func _ready():
-	motion_mode = CharacterBody2D.MOTION_MODE_GROUNDED;
+var attacking = false;
+
+@onready var weapon = $Weapon;
+
+func _weapon_timer_done():
+	attacking = false;
+	weapon.hide();
+	weapon.position = Vector2.ZERO;
 
 func _process(dt: float) -> void:
-	pass
-
-func _physics_process(dt: float) -> void:
-	var input = Input.get_vector("move_left", "move_right", "move_up", "move_down");
-	if input:
-		velocity = input * moveSpeed;
+	var moveInput = Input.get_vector("move_left", "move_right", "move_up", "move_down");
+	if moveInput:
+		velocity = moveInput * moveSpeed;
 	else:
 		velocity = Vector2.ZERO;
-	#if Input.is_action_pressed("move_up"):
-		#velocity = Vector2.UP * moveSpeed;
-	#elif Input.is_action_pressed("move_down"):
-		#velocity = Vector2.DOWN * moveSpeed;
-	#elif Input.is_action_pressed("move_left"):
-		#velocity = Vector2.LEFT * moveSpeed;
-	#elif Input.is_action_pressed("move_right"):
-		#velocity = Vector2.RIGHT * moveSpeed;
-	#else:
-		#velocity = Vector2.ZERO;
+
+	var attackInput = Input.get_vector("attack_left", "attack_right", "attack_up", "attack_down");
+	if attackInput && !attacking:
+		attacking = true;
+		weapon.show();
+		weapon.position = attackInput * 80.0;
+		
+		var timer = Timer.new();
+		add_child(timer);
+		timer.wait_time = 0.5;
+		timer.one_shot = true;
+		timer.start();
+		timer.timeout.connect(_weapon_timer_done);
+
+func _physics_process(dt: float) -> void:
 	move_and_slide();
